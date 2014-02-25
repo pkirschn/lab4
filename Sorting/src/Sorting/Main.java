b321162fb9850887546ad715530063630f8d70f5
ericbai <heyericbai@gmail.com>
2014-02-24 22:35:32 -0500

    Implemented InsertionSort

--- Sorting/src/Sorting/Main.java
+++ Sorting/src/Sorting/Main.java
@@ -22,13 +22,13 @@ public class Main {
 		
 		System.out.println("Sorted Array:");
 		//TODO: sort the array and print out its contents
-		int[] sortedArray = BubbleSort.bubbleSort(unsortedArray);
+
+		int[] results = InsertionSort.insertionSort(unsortedArray); 
 		
-		for (int i sortedArray) {
-			System.out.println(i + " ");
+		for (int i : results) {
+			System.out.print(i + " "); 
 		}
-		System.out.println();
-		
+		System.out.println(); 
 	}
 
 }
